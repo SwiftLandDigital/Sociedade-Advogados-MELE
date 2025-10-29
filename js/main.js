a07f3c1229ce2b50962cbaaea0f9bad99c469e15
Pablo Rafael Caramori Fontes Martin <pablo.rafael.cfm@gmail.com>
2025-10-29 12:22:38 -0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", () => {
+    gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+    // Cria a instância do ScrollSmoother
+    const smoother = ScrollSmoother.create({
+        wrapper: "#smooth-wrapper",
+        content: "#smooth-content",
+        smooth: 1.8,
+        effects: true,
+        normalizeScroll: true,
+        speed: 0.8,
+        smoothTouch: 0.1,
+        ignoreMobileResize: true,
+    });
+
+    // CONSTRUIR OS OBSERVERS
+    const elementsToAnimate = document.querySelectorAll("[data-aos]");
+
+    const observerOptions = {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0,
+    };
+
+    const observerCallback = (entries, observer) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("animate");
+            } else if (entry.boundingClientRect.top > 0) {
+                entry.target.classList.remove("animate");
+            }
+        });
+    };
+
+    const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+    elementsToAnimate.forEach((element) => {
+        observer.observe(element);
+    });
+
+    // ANIMAÇÃO PARALLAX DO BACKGROUND DA HERO-SECTION
+    // const heroBackground = document.querySelector
+});
